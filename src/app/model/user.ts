c39f967faf00afb35457d890a776658129946798
AJ-rodgar <ajrodgar@gmail.com>
2023-06-01 14:31:00 +0200

    Primer commit de Login

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,14 @@
 export class User {
+    constructor(
+        public id: number,
+        public name: string,
+        public lastName: string,
+        public email: string,
+        public job: string,
+        public avatar: string,
+        public password: string,
+        public createdAt: number,
+        public updateAt: number 
+        ){
+    }
 }
