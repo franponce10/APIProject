939ca5bfe223f909a3ae878ce9ba2ce2291c07d5
franponce10 <franseba1996@gmail.com>
2023-06-02 09:49:14 +0200

    Register

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+
+import { Component } from "@angular/core";
+import { User } from "src/app/model/user";
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent {
+  
+  user = new User(0,"","","","","","", Date.now(), Date.now());
 
+  constructor(private authetication: AuthenticationService){}
+    
+
+  register(){
+    
+    this.authetication.register(this.user.email,this.user.password);
+  }
 }
