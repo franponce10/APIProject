5b5dd06fca1d14218298c4d9971ce71b43ad7ba8
AJ-rodgar <ajrodgar@gmail.com>
2023-06-02 09:44:26 +0200

    Creacion CRUD-Service y Register

--- src/app/service/apiservice.service.ts
+++ src/app/service/apiservice.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APIserviceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getListUsers(){
+
+  }
+
+  getSingleUser(id: string){
+    let body = `https://reqres.in/api/users/${id}`;
+    this.http.get<User>(body)
+  }
+
+  getListResources(){
+
+  }
+
+  getResource(){
+
+  }
+
 }
